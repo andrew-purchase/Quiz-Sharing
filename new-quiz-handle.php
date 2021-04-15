5960bff72c18e3ed82e08cafcf1f783e06a3feca
andrew-purchase <61091050+andrew-purchase@users.noreply.github.com>
2021-04-15 00:09:35 -0500

    Update new-quiz-handle.php

--- new-quiz-handle.php
+++ new-quiz-handle.php
@@ -9,37 +9,52 @@
     </style>
 </head>
 <body>
-    <h4>login checking</h4>
-    <?php
+    <h4>Your quiz has been created!</h4>
+    just a moment...
+    <?php 
+
+    $conn = mysqli_connect("localhost:1000", "root", "root", "deleteThis") or die("Connection failed: " . mysqli_connect_error());
+
+//sql statement to find maximum quiz id in database, then +1 to create an unused quiz id
+$sql = "select id from Quiz order by id desc limit 1;";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$quiz_id= $row["id"] + 1;
+
+//now create new quiz object using quiz_id and name
+$sql = "insert into Quiz values($quiz_id, '" . $_REQUEST["name"]. "');";
+$conn->query($sql);
+
 
         //prints everything
-       print_r($_REQUEST);
-       printf("<br><br><br>");
-
-       //prints the name
-        print_r($_REQUEST["name"]);
-       printf("<br><br>");
+        //print_r($_REQUEST);
 
 
         //prints all the questions
         $i = 1;
         while(!empty($_REQUEST["qstn#" . $i])) {
-            print_r($_REQUEST["qstn#" . $i]);
-            printf("<br>");
+            // print_r($_REQUEST["qstn#" . $i]);
+            // printf("<br>");
+            $sql = "INSERT INTO Question VALUES ('quiz" . $quiz_id . "qstn" . $i . "','" .  $_REQUEST["qstn#" . $i]. "'," . $quiz_id .");";
+            $conn->query($sql);
             //prints answers for each question
             $j=1;
             while(!empty($_REQUEST["qstn#" . $i . "ans" . $j])) {
-                print_r($_REQUEST["qstn#" . $i . "ans" . $j]);
-                printf("<br>");
+                // print_r($_REQUEST["qstn#" . $i . "ans" . $j]);
+                if(isset($_REQUEST["qstn#" . $i . "ans" . $j . "cb"]))  //checklists are boolean values (0 | 1)
+                    $b = 1;
+                else $b = 0;
+                $sql = "INSERT INTO Answer VALUES('quiz$quiz_id" . "qstn$i" . "a$j" . "', '" . $_REQUEST["qstn#" . $i . "ans" . $j]. "'," . $b . ", 'quiz" . $quiz_id . "qstn$i'" . ",$quiz_id);";
+                $conn->query($sql);
                 $j++;
             }
             $i++;
-            printf("<br><br>");
        }
-       
+       $sql = "INSERT INTO High_Score VALUES ('empty', 0, " . $quiz_id . ");";
+       $conn->query($sql);
+       mysqli_close($conn);
+    //    echo '<script type="text/javascript">someFunction();</script>'; 
+    echo "<script>setTimeout(function() {window.location.replace('/quiz_project')}, 2000);</script>";
     ?>
-    <script>
-        //redirect to the homepage
-    </script>
 </body>
 </html>
